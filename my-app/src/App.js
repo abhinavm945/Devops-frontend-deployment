1df33c976136917ef7ed3a5773e6b266a3dce458
Abhinav mishra <abhinavm945@gmail.com>
2025-01-14 12:53:43 +0530

    react install and basic frontend

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Finally! its running........</header>
+    </div>
+  );
+}
+
+export default App;
